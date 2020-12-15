5257be2caba56a0ab097c40dd5ad3d6a2c34c218
turtelneck <65428036+turtelneck@users.noreply.github.com>
2020-12-14 21:26:21 -0700

    Create class-assignment.py

---
+++ classes/class-assignment.py
@@ -0,0 +1,14 @@
+
+
+class Guy:
+    name = 'none'
+    relationship_to_user = 'unknown'
+    location = 'unknown'
+
+class tallGuy(Guy):
+    inches = 72
+    age = 'unknown'
+
+class friendlyGuy(Guy):
+    mood: 'good'
+    age = 'unknown'
