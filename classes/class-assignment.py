9aa0920aa5e661d7f04b89761859d6ce5946e1d9
turtelneck <65428036+turtelneck@users.noreply.github.com>
2020-12-16 23:51:25 -0700

    update class-assignment

--- classes/class-assignment.py
+++ classes/class-assignment.py
@@ -1,10 +1,13 @@
 
-
+# creates a class
 class Guy:
+    # the following are class properties
     name = 'none'
     relationship_to_user = 'unknown'
     location = 'unknown'
 
+# these two classes are created with the same properties as the first,
+# but additional properties 'inches' and 'age' are added
 class tallGuy(Guy):
     inches = 72
     age = 'unknown'
