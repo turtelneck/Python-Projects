97e3a8acdbbcae72f6331ecde63f93c62dec7260
turtelneck <65428036+turtelneck@users.noreply.github.com>
2020-12-21 09:19:02 -0700

    Update method-abstraction.py
    added comments

--- classes/method-abstraction.py
+++ classes/method-abstraction.py
@@ -3,12 +3,14 @@ from abc import ABC, abstractmethod
 class Tattoo(ABC):
     def chest(self, thing):
         print("You have a giant",thing,"tattooed on your chest. Classy.")
-
+    # A method in the parent that can be defined
+    # in different ways by subsequent child classes
     @abstractmethod
     def leftBicep(self, thing):
         pass
 
 class AnotherTattoo(Tattoo):
+    # an example of a way to redefine the abstracted method
     def leftBicep(self, thing):
         print("And you have a",thing,"tattooed on your left bicep. Also classy.")
 
