522ef29987b6c836d158d889350c572313bee0e8
turtelneck <65428036+turtelneck@users.noreply.github.com>
2020-12-28 09:16:48 -0700

    Update date_modified_window
    improved variable names, added instructions

--- shutil/file_browser_window.py
+++ shutil/file_browser_window.py
@@ -8,8 +8,11 @@ import shutil, time, os
 window = Tk()
 window.title("Webpage Creator")
 
-# creates a frame to contain lbl, txt, and btn
-# this keeps the content centered if the window expands
+# frames are used to keep all content arranged
+# at the center if the window expands
+introContainer = Frame(window)
+introContainer.pack()
+
 browsingContainer = Frame(window)
 browsingContainer.pack()
 
@@ -21,21 +24,21 @@ executionContainer.pack()
 def browseFiles():
     filename = filedialog.askdirectory()
     # change label contents
-    lbl.configure(text=filename)
+    firstLoc.configure(text=filename)
 
 # identical function for the second button,
 # since using the same one for both while passing
 # in the label as a param had strange consequences
 def browseFiles2():
     filename = filedialog.askdirectory()
-    lbl2.configure(text=filename)
+    secondLoc.configure(text=filename)
 
 
 # moves files between the two folders
 def fileMover():
     # gets the content of the labels
-    source = lbl.cget("text")+"/"
-    destination = lbl2.cget("text")+"/"
+    source = firstLoc.cget("text")+"/"
+    destination = secondLoc.cget("text")+"/"
     # creates a list of the files in `source`
     files = os.listdir(source)
     count = 0
@@ -51,23 +54,37 @@ def fileMover():
             print("--",i)
             count += 1
 
+    info.configure(text="{} files have been moved.\n".format(count))
 
-btn = Button(browsingContainer,anchor=CENTER, text="Browse...", command=browseFiles)
-btn.grid(column=0,row=0, padx=16,pady=16)
+# header/body above file search area
+introHeader = Label(introContainer,anchor=CENTER,text="FileMover 1.0")
+introHeader.configure(font=("Helvetica", 14))
+introHeader.grid(column=0,row=0, padx=16,pady=12)
 
-lbl = Label(browsingContainer,anchor=CENTER,text="this contains your folder address")
-lbl.configure(font=("Helvetica", 9))
-lbl.grid(column=1,row=0, padx=16,pady=16)
+introBody = Label(introContainer,justify=CENTER,wraplength=240,text="All files modified within the last 24 hours will be moved from folder 1 to folder 2")
+introBody.configure(font=("Helvetica", 9))
+introBody.grid(column=0,row=1, padx=16,pady=4)
+# file search area
+browse1 = Button(browsingContainer,anchor=CENTER,text="Browse...", command=browseFiles)
+browse1.grid(column=0,row=0, padx=16,pady=16)
 
-btn = Button(browsingContainer,anchor=CENTER, text="Browse...", command=browseFiles2)
-btn.grid(column=0,row=1, padx=16,pady=16)
+firstLoc = Label(browsingContainer,anchor=CENTER,text="please select the location of folder 1")
+firstLoc.configure(font=("Helvetica", 9))
+firstLoc.grid(column=1,row=0, padx=16,pady=16)
 
-lbl2 = Label(browsingContainer,anchor=CENTER,text="this contains your folder address")
-lbl2.configure(font=("Helvetica", 9))
-lbl2.grid(column=1,row=1, padx=16,pady=16)
+browse2 = Button(browsingContainer,anchor=CENTER,text="Browse...", command=browseFiles2)
+browse2.grid(column=0,row=1, padx=16,pady=16)
 
-btn = Button(executionContainer,anchor=CENTER, text="Move My Files!", command=fileMover)
-btn.grid(column=0,row=0, padx=16,pady=16)
+secondLoc = Label(browsingContainer,anchor=CENTER,text="please select the location of folder 2")
+secondLoc.configure(font=("Helvetica", 9))
+secondLoc.grid(column=1,row=1, padx=16,pady=16)
+# executor button and all else below that
+exec = Button(executionContainer,anchor=CENTER, text="Move My Files!", command=fileMover)
+exec.grid(column=0,row=0, padx=16,pady=16)
+
+info = Label(executionContainer,anchor=CENTER, text="")
+info.configure(font=("Helvetica", 9))
+info.grid(column=0,row=1, padx=16,pady=16)
 
 
 
